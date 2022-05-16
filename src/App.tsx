bd2cc6942ed2d7d9dc71164ae4ca2491e2da2c26
Krzysztof C <krzysztof.c.krzysztof@gmail.com>
2022-05-16 16:08:04 +0200

    Basic structure of main view. Added search field and button. Added add ad button. Added title h1. Basic CSS config.

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {TestInterface} from "types";
+import "./App.css";
+export const App = () => {
 
-function App() {
-  const foobar: TestInterface = {
-    x: 123,
-    y: "test",
-    z: true,
-  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+          <header>
+              <h1>
+                  <strong>Mapa </strong> Ogłoszeń
+              </h1>
+              <button>Dodaj ogłoszenie</button>
+              <div className="search">
+                  <input type="text"/>
+                  <button>Szukaj</button>
+              </div>
+          </header>
+          <div className="map">
+
+          </div>
+      </>
   );
 }
 
-export default App;
